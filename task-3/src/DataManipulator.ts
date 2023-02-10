86d6303b729e20815eafef7f87e2588020ee9450
Phuong Vu <100975883+phuongv8@users.noreply.github.com>
2023-02-09 22:13:24 -0800

    Update schema to match with graph

--- task-3/src/DataManipulator.ts
+++ task-3/src/DataManipulator.ts
@@ -1,20 +1,23 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+  price_abc: number;
+  price_def: number;
+  ratio: number;
+  timestamp: Date;
+  upper_bound: number;
+  lower_bound: number;
+  trigger_alert: number | undefined;
 }
-
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
     return serverResponds.map((el: any) => {
       return {
         stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
+        top_ask_price: (el.top_ask && el.top_ask.price) || 0,
         timestamp: el.timestamp,
       };
-    })
+    });
   }
 }
